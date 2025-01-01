c93cae4481cec4f56652bf4aab3a76ef24782bc3
Jared Crean <jcrean01@gmail.com>
2024-12-31 17:05:19 -0700

    add ability to rotate the indices of blocks

--- src/mesh/structured_block.h
+++ src/mesh/structured_block.h
@@ -10,15 +10,22 @@ namespace mesh {
 
 struct MeshBlockSpec
 {
+  // Note: num_cells_x and num_cells_y are the number of cells in each
+  //       direction in a rotation=0 coordinate system.
+  //       The rotation argument changes the block-local coordinate
+  //       system but does not change the number of cells in each direction.
+  //       This allows specifying a multi-block mesh using a rotation=0
+  //       global coordinate system
+  //       The coord_func should also be defined in the rotation=0 global
+  //       coordinate system, and should map from the unit square into
+  //       the desired shape
   MeshBlockSpec(UInt num_cells_x, UInt num_cells_y, UInt rotation,
-                double xmin, double xmax, double ymin, double ymax) :
+                std::function<std::array<double, 2>(double x, double y)> coord_func = 
+                  [](double x, double y) {return std::array<double, 2>{x, y};}) :
     num_cells_x(num_cells_x),
     num_cells_y(num_cells_y),
     rotation(rotation),
-    coord_func([=](UInt i, UInt j, UInt num_verts_x, UInt num_verts_y)
-    { return std::array<double, 2>{i * (xmax - xmin)/(num_verts_x - 1),
-                                   j * (ymax - ymin)/(num_verts_y - 1)};
-    })
+    coord_func(coord_func)
   {}
 
   UInt num_cells_x;
@@ -26,7 +33,7 @@ struct MeshBlockSpec
 
   UInt rotation;  // 0 = no rotation, 1 = rotate 90 degrees counter-clockwise, 2 = 180 degrees, 3 = 270 degrees
                   // this allows testing the transform between block interfaces
-  std::function<std::array<double, 2>(UInt i, UInt j, UInt numVertsX, UInt numVertsY)> coord_func;
+  std::function<std::array<double, 2>(double x, double y)> coord_func;
 };
 
 enum class BlockType
@@ -35,24 +42,49 @@ enum class BlockType
   GhostBC
 };
 
+// given an index (i, j) in the standard orientation (ie rotation=0), compute
+// the location of the corresponding point under the rotation
+std::pair<UInt, UInt> applyRotation(UInt rotation, UInt i, UInt j, UInt dimx,  UInt dimy)
+{
+  switch (rotation)
+  {
+    case 0: { return {i,   j}; }
+    case 1: { return {j,  dimx -1 - i}; }
+    case 2: { return {dimx -1 - i, dimy -1 - j}; }
+    case 3: { return {dimy -1 - j, i}; }
+    default: { throw std::runtime_error("invalid rotation"); }
+  }
+}
+
 class StructuredBlock
 {
   public:
     StructuredBlock(const MeshBlockSpec& spec, UInt block_id, BlockType block_type) :
       m_block_id(block_id),
       m_block_type(block_type),
-      m_owned_cell_range(0, spec.num_cells_x-1, 0, spec.num_cells_y-1),
-      m_owned_vert_range(0, spec.num_cells_x, 0, spec.num_cells_y),
-      m_owned_vert_coords("block_coords", spec.num_cells_x+1, spec.num_cells_y+1),
+      m_owned_cell_range(0, spec.rotation % 2 == 0 ? spec.num_cells_x-1 : spec.num_cells_y-1,
+                         0, spec.rotation % 2 == 0 ? spec.num_cells_y-1 : spec.num_cells_x-1),
+      m_owned_vert_range(0, spec.rotation % 2 == 0 ? spec.num_cells_x : spec.num_cells_y, 
+                         0, spec.rotation % 2 == 0 ? spec.num_cells_y : spec.num_cells_x),
+      m_owned_vert_coords("block_coords", spec.rotation % 2 == 0 ? spec.num_cells_x+1 : spec.num_cells_y+1,
+                                          spec.rotation % 2 == 0 ? spec.num_cells_y+1 : spec.num_cells_x+1),
       m_offset_into_block{0, 0},
-      m_all_block_size{spec.num_cells_x, spec.num_cells_y}
-    {
-      for (UInt i : m_owned_vert_range.getXRange())
-        for (UInt j : m_owned_vert_range.getYRange())
+      m_all_block_size{spec.rotation % 2 == 0 ? spec.num_cells_x : spec.num_cells_y,
+                       spec.rotation % 2 == 0 ? spec.num_cells_y : spec.num_cells_x}
+    { 
+      UInt dimx = spec.num_cells_x + 1;
+      UInt dimy = spec.num_cells_y + 1;
+
+      for (UInt i : Range(0, dimx-1))
+        for (UInt j : Range(0, dimy-1))
         {
-          std::array<double, 2> coords = spec.coord_func(i, j, m_owned_vert_range.getXRange().size(), m_owned_vert_range.getYRange().size());
-          m_owned_vert_coords(i, j, 0) = coords[0];
-          m_owned_vert_coords(i, j, 1) = coords[1];
+          auto [iprime, jprime] = applyRotation(spec.rotation, i, j, dimx, dimy);
+          double x = double(i) / (dimx - 1);
+          double y = double(j) / (dimy - 1);
+          auto [xprime, yprime] = spec.coord_func(x, y);
+
+          m_owned_vert_coords(iprime, jprime, 0) = xprime;
+          m_owned_vert_coords(iprime, jprime, 1) = yprime;
         }
     }
 
--- test/unit/test_structured_block.cc
+++ test/unit/test_structured_block.cc
@@ -14,7 +14,8 @@ std::array<UInt, 2> make_array(const std::array<UInt, 2>& arr)
 
 TEST(StructuredBlock, SimpleBlock)
 {
-  MeshBlockSpec spec(2, 4, 0, 0.0, 1.0, 0.0, 2.0);
+  auto coord_func = [](double x, double y) { return std::array<double, 2>{x, 4*y}; };
+  MeshBlockSpec spec(2, 4, 0, coord_func);
   StructuredBlock block(spec, 2, BlockType::Regular);
 
   EXPECT_EQ(block.getBlockId(), 2);
@@ -28,7 +29,7 @@ TEST(StructuredBlock, SimpleBlock)
   EXPECT_EQ(coords.extent(0), 3);
   EXPECT_EQ(coords.extent(1), 5);
   EXPECT_EQ(coords.extent(2), 2);
-  
+
   EXPECT_DOUBLE_EQ(coords(0, 0, 0), 0);
   EXPECT_DOUBLE_EQ(coords(0, 0, 1), 0);
 
@@ -40,11 +41,130 @@ TEST(StructuredBlock, SimpleBlock)
 
 
   EXPECT_DOUBLE_EQ(coords(0, 1, 0), 0);
-  EXPECT_DOUBLE_EQ(coords(0, 1, 1), 0.5);
+  EXPECT_DOUBLE_EQ(coords(0, 1, 1), 1.0);
 
   EXPECT_DOUBLE_EQ(coords(1, 1, 0), 0.5);
-  EXPECT_DOUBLE_EQ(coords(1, 1, 1), 0.5); 
+  EXPECT_DOUBLE_EQ(coords(1, 1, 1), 1.0); 
 
   EXPECT_DOUBLE_EQ(coords(2, 1, 0), 1.0);
-  EXPECT_DOUBLE_EQ(coords(2, 1, 1), 0.5);    
+  EXPECT_DOUBLE_EQ(coords(2, 1, 1), 1.0);    
+}
+
+TEST(StructuredBlock, applyRotation0)
+{
+  UInt rotation = 0;
+  UInt dimx = 5;
+  UInt dimy = 4;
+  EXPECT_EQ(applyRotation(rotation, 0, 0, dimx, dimy), std::make_pair(UInt(0), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 4, 0, dimx, dimy), std::make_pair(UInt(4), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 0, 3, dimx, dimy), std::make_pair(UInt(0), UInt(3)));
+  EXPECT_EQ(applyRotation(rotation, 2, 1, dimx, dimy), std::make_pair(UInt(2), UInt(1)));
+}
+
+TEST(StructuredBlock, applyRotation1)
+{
+  UInt rotation = 1;
+  UInt dimx = 5;
+  UInt dimy = 4;
+  EXPECT_EQ(applyRotation(rotation, 0, 0, dimx, dimy), std::make_pair(UInt(0), UInt(4)));
+  EXPECT_EQ(applyRotation(rotation, 4, 0, dimx, dimy), std::make_pair(UInt(0), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 0, 3, dimx, dimy), std::make_pair(UInt(3), UInt(4)));
+  EXPECT_EQ(applyRotation(rotation, 2, 1, dimx, dimy), std::make_pair(UInt(1), UInt(2)));
+}
+
+TEST(StructuredBlock, applyRotation2)
+{
+  UInt rotation = 2;
+  UInt dimx = 5;
+  UInt dimy = 4;
+  EXPECT_EQ(applyRotation(rotation, 0, 0, dimx, dimy), std::make_pair(UInt(4), UInt(3)));
+  EXPECT_EQ(applyRotation(rotation, 4, 0, dimx, dimy), std::make_pair(UInt(0), UInt(3)));
+  EXPECT_EQ(applyRotation(rotation, 0, 3, dimx, dimy), std::make_pair(UInt(4), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 2, 1, dimx, dimy), std::make_pair(UInt(2), UInt(2)));
+}
+
+TEST(StructuredBlock, applyRotation3)
+{
+  UInt rotation = 3;
+  UInt dimx = 5;
+  UInt dimy = 4;
+  EXPECT_EQ(applyRotation(rotation, 0, 0, dimx, dimy), std::make_pair(UInt(3), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 4, 0, dimx, dimy), std::make_pair(UInt(3), UInt(4)));
+  EXPECT_EQ(applyRotation(rotation, 0, 3, dimx, dimy), std::make_pair(UInt(0), UInt(0)));
+  EXPECT_EQ(applyRotation(rotation, 2, 1, dimx, dimy), std::make_pair(UInt(2), UInt(2)));
+}
+
+TEST(StructuredBlock, SimpleBlockTransform1)
+{
+  auto coord_func = [](double x, double y) { return std::array<double, 2>{x, 4*y}; };
+  MeshBlockSpec spec(2, 4, 1, coord_func);
+  StructuredBlock block(spec, 2, BlockType::Regular);
+
+  EXPECT_EQ(block.getBlockId(), 2);
+  EXPECT_EQ(block.getBlockType(), BlockType::Regular);
+  EXPECT_EQ(block.getOwnedVerts(), Range2D(0, 4, 0, 2));
+  EXPECT_EQ(block.getOwnedCells(), Range2D(0, 3, 0, 1));
+  EXPECT_EQ(block.getOffsetIntoBlock(), make_array({0, 0}));
+  EXPECT_EQ(block.getAllBlockSize(), make_array({4, 2}));
+
+  auto coords = block.getOwnedVertCoords();
+  EXPECT_EQ(coords.extent(0), 5);
+  EXPECT_EQ(coords.extent(1), 3);
+  EXPECT_EQ(coords.extent(2), 2);
+
+  EXPECT_DOUBLE_EQ(coords(0, 0, 0), 1);
+  EXPECT_DOUBLE_EQ(coords(0, 0, 1), 0);
+
+  EXPECT_DOUBLE_EQ(coords(4, 2, 0), 0);
+  EXPECT_DOUBLE_EQ(coords(4, 2, 1), 4);  
+}
+
+TEST(StructuredBlock, SimpleBlockTransform2)
+{
+  auto coord_func = [](double x, double y) { return std::array<double, 2>{x, 4*y}; };
+  MeshBlockSpec spec(2, 4, 2, coord_func);
+  StructuredBlock block(spec, 2, BlockType::Regular);
+
+  EXPECT_EQ(block.getBlockId(), 2);
+  EXPECT_EQ(block.getBlockType(), BlockType::Regular);
+  EXPECT_EQ(block.getOwnedVerts(), Range2D(0, 2, 0, 4));
+  EXPECT_EQ(block.getOwnedCells(), Range2D(0, 1, 0, 3));
+  EXPECT_EQ(block.getOffsetIntoBlock(), make_array({0, 0}));
+  EXPECT_EQ(block.getAllBlockSize(), make_array({2, 4}));
+
+  auto coords = block.getOwnedVertCoords();
+  EXPECT_EQ(coords.extent(0), 3);
+  EXPECT_EQ(coords.extent(1), 5);
+  EXPECT_EQ(coords.extent(2), 2);
+
+  EXPECT_DOUBLE_EQ(coords(0, 0, 0), 1);
+  EXPECT_DOUBLE_EQ(coords(0, 0, 1), 4);
+
+  EXPECT_DOUBLE_EQ(coords(2, 4, 0), 0);
+  EXPECT_DOUBLE_EQ(coords(2, 4, 1), 0);  
+}
+
+TEST(StructuredBlock, SimpleBlockTransform3)
+{
+  auto coord_func = [](double x, double y) { return std::array<double, 2>{x, 4*y}; };
+  MeshBlockSpec spec(2, 4, 3, coord_func);
+  StructuredBlock block(spec, 2, BlockType::Regular);
+
+  EXPECT_EQ(block.getBlockId(), 2);
+  EXPECT_EQ(block.getBlockType(), BlockType::Regular);
+  EXPECT_EQ(block.getOwnedVerts(), Range2D(0, 4, 0, 2));
+  EXPECT_EQ(block.getOwnedCells(), Range2D(0, 3, 0, 1));
+  EXPECT_EQ(block.getOffsetIntoBlock(), make_array({0, 0}));
+  EXPECT_EQ(block.getAllBlockSize(), make_array({4, 2}));
+
+  auto coords = block.getOwnedVertCoords();
+  EXPECT_EQ(coords.extent(0), 5);
+  EXPECT_EQ(coords.extent(1), 3);
+  EXPECT_EQ(coords.extent(2), 2);
+
+  EXPECT_DOUBLE_EQ(coords(0, 0, 0), 0);
+  EXPECT_DOUBLE_EQ(coords(0, 0, 1), 4);
+
+  EXPECT_DOUBLE_EQ(coords(4, 2, 0), 1);
+  EXPECT_DOUBLE_EQ(coords(4, 2, 1), 0);  
 }
