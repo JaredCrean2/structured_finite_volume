d510a1ab0bde6cfe5a77487557399ed069b85b9a
Jared Crean <jcrean01@gmail.com>
2025-06-19 18:13:15 -0600

    add replacement for std::array

---
+++ src/utils/vec.h
@@ -0,0 +1,194 @@
+#ifndef STRUCTURED_FINITE_VOLUME_UTILS_VEC_H
+#define STRUCTURED_FINITE_VOLUME_UTILS_VEC_H
+
+#include "project_defs.h"
+#include "utils/error_handling.h"
+#include <algorithm>
+#include <iostream>
+
+namespace structured_fv {
+
+// this is a replacement for std::array that will work on device eventually
+template <typename T, UInt N>
+struct Vec
+{
+  using value_type = T;
+  using size_type = UInt;
+  using difference_type = std::ptrdiff_t;
+  using reference = T&;
+  using const_reference = const T&;
+  using pointer = T*;
+  using const_pointer = const T*;
+  using iterator = T*;
+  using const_iterator = const T*;
+
+  // constructors use aggregate initialization
+  constexpr reference at(UInt pos)
+  {
+    assertAlways(pos < N, "bounds error");
+    return _m_vals[pos];
+  }
+
+  constexpr const_reference at(UInt pos) const
+  {
+    assertAlways(pos < N, "bounds error");
+    return _m_vals[pos];
+  }
+
+  constexpr reference operator[](UInt pos) 
+  {
+    assert(pos < N);
+    return _m_vals[pos];
+  }
+
+  constexpr const_reference operator[](UInt pos) const
+  {
+    assert(pos < N);
+    return _m_vals[pos];
+  }
+
+  constexpr reference front()
+  {
+    static_assert(N > 0);
+    return _m_vals[0];
+  }
+
+  constexpr const_reference front() const
+  {
+    static_assert(N > 0);
+    return _m_vals[0];
+  }
+
+  constexpr reference back()
+  {
+    static_assert(N > 0);
+    return _m_vals[N-1];
+  }
+
+  constexpr const_reference back() const
+  {
+    static_assert(N > 0);
+    return _m_vals[N-1];
+  }
+
+  constexpr pointer data() noexcept { return _m_vals; }
+
+  constexpr const_pointer data() const noexcept { return _m_vals; }
+
+  constexpr iterator begin() noexcept { return _m_vals; }
+
+  constexpr const_iterator begin() const noexcept { return _m_vals; }
+
+  constexpr const_iterator cbegin() const noexcept { return _m_vals; }
+
+  constexpr iterator end() noexcept { return begin() + N; }
+
+  constexpr const_iterator end() const noexcept { return begin() + N; }
+
+  constexpr const_iterator cend() const noexcept { return begin() + N; }
+
+  constexpr bool empty() const noexcept { return N == 0; }
+
+  constexpr size_type size() const noexcept { return N; }
+
+  constexpr size_type max_size() const noexcept { return N; }
+
+  constexpr void fill(const T& val) noexcept
+  {
+    for (UInt i=0; i < N; ++i)
+      _m_vals[i] = val;
+  }
+
+  constexpr void swap(Vec<T, N>& other) noexcept
+  {
+    for (UInt i=0; i < N; ++i)
+    {
+      T tmp = _m_vals[i];
+      _m_vals[i] = other[i];
+      other[i] = tmp;
+    }
+  }
+
+
+  T _m_vals[N];  // this can't be private if the class is an aggregate, but no one
+                 // should access it directly
+};
+
+// std::array is only required to implement comparisons for arrays of the same T and
+// N, which is a little weird
+template <typename T, UInt N>
+bool operator==(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template <typename T, UInt N>
+bool operator!=(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return !(lhs == rhs);
+}
+
+template <typename T, UInt N>
+bool operator<(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template <typename T, UInt N>
+bool operator<=(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return (lhs < rhs) || (lhs == rhs);
+}
+
+template <typename T, UInt N>
+bool operator>(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return !(lhs <= rhs);
+}
+
+template <typename T, UInt N>
+bool operator>=(const Vec<T, N>& lhs, const Vec<T, N>& rhs)
+{
+  return !(lhs < rhs);
+}
+
+template <size_t I, typename T, UInt N>
+constexpr T& get(Vec<T, N>& arr)
+{
+  return arr[I];
+}
+
+template <size_t I, typename T, UInt N>
+constexpr const T& get(const Vec<T, N>& arr)
+{
+  return arr[I];
+}
+
+template <typename T, UInt N>
+std::ostream& operator<<(std::ostream& os, const Vec<T, N>& arr)
+{
+  for (UInt i=0; i < N; ++i)
+  {
+    os << arr[i];
+    if (i < N-1)
+      os << ", ";
+  }
+
+  return os;
+}
+
+template <typename T>
+using Vec2 = Vec<T, 2>;
+
+template <typename T>
+using Vec3 = Vec<T, 3>;
+
+template <typename T>
+using Vec4 = Vec<T, 4>;
+
+template <typename T>
+using Vec5 = Vec<T, 5>;
+
+}
+
+#endif
---
+++ test/unit/test_vec.cc
@@ -0,0 +1,256 @@
+#include "gtest/gtest.h"
+#include "utils/vec.h"
+
+namespace {
+using Vec3 = structured_fv::Vec<int, 3>;
+/*
+void expect_eq(const Vec3& arr1, const std::array<int, 3>& arr2)
+{
+  EXPECT_EQ(arr1[0], arr2[0]);
+  EXPECT_EQ(arr1[1], arr2[1]);
+  EXPECT_EQ(arr1[2], arr2[2]);
+}
+*/
+
+void expect_eq(const Vec3& arr1, const Vec3& arr2)
+{
+  EXPECT_EQ(arr1[0], arr2[0]);
+  EXPECT_EQ(arr1[1], arr2[1]);
+  EXPECT_EQ(arr1[2], arr2[2]);  
+}
+
+}
+
+TEST(Vec, Constructor)
+{
+  Vec3 v1{1, 2, 3};
+  EXPECT_EQ(v1[0], 1);
+  EXPECT_EQ(v1[1], 2);
+  EXPECT_EQ(v1[2], 3);  
+
+  Vec3 v2 = {1, 2, 3};
+  expect_eq(v2, {1, 2, 3});
+
+  Vec3 v3(v2);
+  expect_eq(v3, {1, 2, 3});
+
+  Vec3 v4(std::move(v2));
+  expect_eq(v4, {1, 2, 3});
+}
+
+
+TEST(Vec, Assignment)
+{
+  Vec3 v1{1, 2, 3};
+
+  Vec3 v2 = v1;
+  expect_eq(v2, {1, 2, 3});
+
+  Vec3 v3 = std::move(v1);
+  expect_eq(v3, {1, 2, 3});
+}
+
+TEST(Vec, ConstAssignment)
+{
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+
+  Vec3 v2 = v2_const;
+  expect_eq(v2, {1, 2, 3});
+
+  Vec3 v3 = std::move(v2_const);
+  expect_eq(v3, {1, 2, 3});
+}
+
+TEST(Vec, Indexing)
+{
+  Vec3 v1{1, 2, 3};
+  expect_eq(v1, {1, 2, 3});
+
+  v1[1] = 4;
+  expect_eq(v1, {1, 4, 3});
+
+  v1.at(1) = 5;
+  expect_eq(v1, {1, 5, 3});
+
+  EXPECT_ANY_THROW(v1.at(3));
+}
+
+TEST(Vec, ConstIndexing)
+{
+  Vec3 v1{1, 2, 3};
+  expect_eq(v1, {1, 2, 3});
+
+  const Vec3& v2_const = v1;
+  EXPECT_EQ(v2_const[1], 2);
+  EXPECT_EQ(v2_const.at(1), 2);
+  EXPECT_ANY_THROW(v2_const.at(3));
+}
+
+TEST(Vec, Front)
+{
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+
+  EXPECT_EQ(v1.front(), 1);
+  EXPECT_EQ(v2_const.front(), 1);
+
+  v1.front() = 4;
+  EXPECT_EQ(v1.front(), 4);
+  EXPECT_EQ(v2_const.front(), 4);
+}
+
+TEST(Vec, Back)
+{
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+
+  EXPECT_EQ(v1.back(), 3);
+  EXPECT_EQ(v2_const.back(), 3);
+
+  v1.back() = 4;
+  EXPECT_EQ(v1.back(), 4);
+  EXPECT_EQ(v2_const.back(), 4);
+}
+
+TEST(Vec, Data)
+{
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+
+  EXPECT_EQ(v1.data(), &(v1[0]));
+  EXPECT_EQ(v2_const.data(), &(v2_const[0]));
+}
+
+TEST(Vec, Iterators)
+{
+  std::vector<int> copy1, copy2, copy3;
+
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+  for (auto val : v1)
+    copy1.push_back(val);
+
+  for (auto val : v2_const)
+    copy2.push_back(val);
+
+  for (auto it = v2_const.cbegin(); it != v2_const.cend(); ++it)
+    copy3.push_back(*it);
+
+  EXPECT_EQ(copy1.size(), 3U);
+  EXPECT_EQ(copy2.size(), 3U);
+  EXPECT_EQ(copy3.size(), 3U);
+  for (int i=0; i < 3; ++i)
+  {
+    EXPECT_EQ(copy1[i], v1[i]);
+    EXPECT_EQ(copy2[i], v1[i]);
+    EXPECT_EQ(copy3[i], v1[i]);
+  }
+}
+
+TEST(Vec, Empty)
+{
+  Vec3 v1{1, 2, 3};
+
+  EXPECT_FALSE(v1.empty());
+  structured_fv::Vec<int, 0> v2{};
+  EXPECT_TRUE(v2.empty());
+}
+
+TEST(Vec, Size)
+{
+  Vec3 v1{1, 2, 3};
+  EXPECT_EQ(v1.size(), 3U);
+  EXPECT_EQ(v1.max_size(), 3U);
+}
+
+TEST(Vec, Fill)
+{
+  Vec3 v1{1, 2, 3};
+  v1.fill(4);
+  expect_eq(v1, {4, 4, 4});
+}
+
+TEST(Vec, Swap)
+{
+  Vec3 v1{1, 2, 3}, v2{4, 5, 6};
+  auto it1 = v1.begin();
+  auto it2 = v2.begin();
+  EXPECT_EQ(*it1, 1);
+  EXPECT_EQ(*it2, 4);
+
+  v1.swap(v2);
+  expect_eq(v1, {4, 5, 6});
+  expect_eq(v2, {1, 2, 3});
+
+  EXPECT_EQ(*it1, 4);
+  EXPECT_EQ(*it2, 1);
+}
+
+TEST(Vec, Equality)
+{
+  Vec3 v1{1, 2, 3}, v2{4, 5, 6}, v3{1, 2, 3};
+  EXPECT_FALSE(v1 == v2);
+  EXPECT_TRUE(v1 == v3);
+
+  EXPECT_TRUE(v1 != v2);
+  EXPECT_FALSE(v1 != v3);
+}
+
+TEST(Vec, LessThan)
+{
+  Vec3 v1{1, 2, 3}, v2{0, 1, 2}, v3{1, 2, 3}, v4{2, 3, 4};
+  EXPECT_FALSE(v1 < v2);
+  EXPECT_FALSE(v1 < v3);
+  EXPECT_TRUE(v1 < v4);
+}
+
+TEST(Vec, LessThanOrEqul)
+{
+  Vec3 v1{1, 2, 3}, v2{0, 1, 2}, v3{1, 2, 3}, v4{2, 3, 4};
+  EXPECT_FALSE(v1 < v2);
+  EXPECT_TRUE(v1 <= v3);
+  EXPECT_TRUE(v1 < v4);
+}
+
+TEST(Vec, GreaterThan)
+{
+  Vec3 v1{1, 2, 3}, v2{0, 1, 2}, v3{1, 2, 3}, v4{2, 3, 4};
+  EXPECT_TRUE(v1 > v2);
+  EXPECT_FALSE(v1 > v3);
+  EXPECT_FALSE(v1 > v4);
+}
+
+TEST(Vec, GreaterThanOrEqual)
+{
+  Vec3 v1{1, 2, 3}, v2{0, 1, 2}, v3{1, 2, 3}, v4{2, 3, 4};
+  EXPECT_TRUE(v1 >= v2);
+  EXPECT_TRUE(v1 >= v3);
+  EXPECT_FALSE(v1 >= v4);
+}
+
+TEST(Vec, Get)
+{
+  Vec3 v1{1, 2, 3};
+  const Vec3& v2_const = v1;
+
+  EXPECT_EQ(structured_fv::get<0>(v1), 1);
+  EXPECT_EQ(structured_fv::get<1>(v1), 2);
+  EXPECT_EQ(structured_fv::get<2>(v1), 3);
+
+
+  EXPECT_EQ(structured_fv::get<0>(v2_const), 1);
+  EXPECT_EQ(structured_fv::get<1>(v2_const), 2);
+  EXPECT_EQ(structured_fv::get<2>(v2_const), 3);
+
+  structured_fv::get<0>(v1) = 4;
+  EXPECT_EQ(structured_fv::get<0>(v1), 4);
+}
+
+TEST(Vec, OutputOperator)
+{
+  std::stringstream ss;
+  Vec3 v1{1, 2, 3};
+  ss << v1;
+  EXPECT_EQ(ss.str(), "1, 2, 3");
+}
